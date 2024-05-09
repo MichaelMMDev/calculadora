6f61a9374d79abc1be2a26deffd865d9b064575c
JemmEdu16 <159553412+MichaelMMDev@users.noreply.github.com>
2024-05-09 10:05:41 -0300

    tgy
    fgyf

---
+++ estruturas_de_repetição/dicio_em_conjunto_com_um_loop_for.py
@@ -0,0 +1,3 @@
+estoque_frutas={"abacaxi":10, "morango":20, "uva":15}
+for fruta, quantidade in estoque_frutas.items():
+    print(f'fruta:{fruta}, quantidade:{quantidade}')
---
+++ estruturas_de_repetição/pass.py
@@ -0,0 +1,6 @@
+minha_liosta=[1,2,3,4,5]
+for item in minha_liosta:
+    if item % 2 ==0:
+        pass
+    else:
+        print("impar:",item)
